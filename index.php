b28ae9e7e0485f1749bc5517a336037dc11fe9d5
ralphkakes <99045360@mydavinci.nl>
2020-04-29 23:34:22 +0200

    Test

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>ToDoList</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  <h1>Yeet</h1>
+</body>
+</html>
+
