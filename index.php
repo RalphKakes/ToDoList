edc0fe21404ea7169fc74d66a6728290f54af1db
ralphkakes <99045360@mydavinci.nl>
2020-04-29 23:58:37 +0200

    Database connection

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
-
+// include the database.php file for connection function//
+include "db.php";
 ?>
 <!doctype html>
 
 <html lang="en">
 <head>
-  <meta charset="utf-8">
+    <meta charset="utf-8">
 
-  <title>ToDoList</title>
-  <meta name="description" content="The HTML5 Herald">
-  <meta name="author" content="SitePoint">
-
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
-
+    <title>ToDoList</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 
 <body>
-  <h1>Yeet</h1>
+<h1>Yeet</h1>
 </body>
 </html>
 
